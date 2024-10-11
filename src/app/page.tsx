133968ebc43be1a26f6913d167fb86a728c5a1a6
saul <luckly083@gmail.com>
2024-10-10 22:08:23 -0500

    Added my personal information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -165,11 +165,11 @@ export default function Page() {
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   During my time in university, I attended{" "}
-                  {DATA.hackathons.length}+ hackathons. People from around the
-                  country would come together and build incredible things in 2-3
-                  days. It was eye-opening to see the endless possibilities
-                  brought to life by a group of motivated and passionate
-                  individuals.
+                  {DATA.hackathons.length}+ hackathons and conferences. People
+                  from around the world would come together and build incredible
+                  things in 2-3 days. It was eye-opening to see the endless
+                  possibilities brought to life by a group of motivated and
+                  passionate individuals.
                 </p>
               </div>
             </div>
